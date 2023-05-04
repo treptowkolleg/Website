a65c6420b847388b52ac9062f118240af11186f8
Benjamin Wagner <bwagner@vapita.de>
2023-05-04 21:29:59 +0200

    change index.html to index.php

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<html lang="de">
+<head>
+    <meta  charset="utf-8">
+    <meta name=author content="Vico">
+    <meta name="description" content="Inhalt">
+    <meta name="keywords" content="Test">
+    <title> Hallo</title>
+</head>
+<body>
+
+<!-- Umlaute werden codiert: U = &Uuml; oder ö = &ouml; -->
+<h1> Das ist eine &Uuml;berschrift</h1>
+
+
+<p>Hier k&ouml;nnte ein <a href= "/?id=5">Text</a> stehen</p>
+<a href="/" >Hier lang!</a>
+<p>Heute ist der <?=date("d.m.Y")?>. Ist das nicht toll?</p>
+</body>
+</html>
